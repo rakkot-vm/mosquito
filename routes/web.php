f9a9bea44e7b9ff81e57cd97c71b4db444fbd034
Vitaliy Mezhebitskiy <valet89vm@gmail.com>
2019-01-23 17:42:49 +0200

    auth in admin (inprogress)

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,4 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
-
-//Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/admin', 'AdminController@index')->name('admin');
